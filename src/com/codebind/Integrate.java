a65efdc3d81676de67216da735a3daacd2b87943
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2022-03-12 13:10:21 -0800

    added method to check continuity + helper methods

--- src/com/codebind/Integrate.java
+++ src/com/codebind/Integrate.java
@@ -40,6 +40,72 @@ public class Integrate extends EquationParser{
         return averageArray(y);
     }
 
+    public boolean isContinuousStrict()
+    {
+        ArrayList<Double> linear = linearSubDivision(leftBound, rightBound, 10000);
+        double dx = 0.00001;
+        ArrayList<Integer> indexes = findAllVariableIndex();
+
+        double y1 = 0.0;
+        double y2 = 0.0;
+
+        ListIterator<Double> enumerateX = linear.listIterator();
+        while (enumerateX.hasNext())
+        {
+            double val = enumerateX.next();
+
+            replaceVariables(indexes, val);
+            y1 = this.evaluate();
+
+            replaceVariables(indexes, val + dx);
+            y2 = this.evaluate();
+
+            if (Math.abs(y2 - y1) > 0.001)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void replaceVariables(ArrayList<Integer> indexes, double value)
+    {
+        for (Integer index : indexes)
+        {
+            this.getRPN().get(index).setValue(value);
+        }
+    }
+
+    private ArrayList<Integer> findAllVariableIndex()
+    {
+        ArrayList<Integer> indexes = new ArrayList<>();
+        ListIterator<Token> enumerateEqn = this.getRPN().listIterator();
+        while (enumerateEqn.hasNext())
+        {
+            if (enumerateEqn.next().getTxt().equals(Token.Variable))
+            {
+                indexes.add(enumerateEqn.previousIndex());
+            }
+        }
+
+        return indexes;
+    }
+
+
+
+    public static ArrayList<Double> linearSubDivision(double start, double end, int subDivisions)
+    {
+        ArrayList<Double> lin = new ArrayList<>();
+        double dx = (end - start) / (subDivisions - 1);
+        double num = start;
+        for (int i  = 0; i < subDivisions; i++)
+        {
+            lin.add(num);
+            num += dx;
+        }
+        return lin;
+    }
+
     private static Double averageArray(ArrayList<Double> arr)
     {
         Double sum = 0.0;
